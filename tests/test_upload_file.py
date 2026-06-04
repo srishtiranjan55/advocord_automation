1c5fe795b4600996a3f4e49d3bbf0b6b46c59ca0
srishtiranjan55 <ranjansrishti55@gmail.com>
2026-06-04 12:35:28 +0530

    resolve upload file issue

--- tests/test_upload_file.py
+++ tests/test_upload_file.py
@@ -3,7 +3,9 @@ from playwright.sync_api import expect
 
 def test_upload_file(logged_in_page):
     page = logged_in_page
+    page.wait_for_timeout(5000)
     page.locator(".depedentArrowCircle").click()
+    page.wait_for_timeout(5000)
     page.locator(".navDropdownLabel", has_text="Documents").click()
 
     page.locator("#fileUploadModal").set_input_files(
